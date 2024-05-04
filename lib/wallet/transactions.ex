4ff1b2ba5a1ce49e49fadee099151e2decca86ea
Hebert <hebertsouza87@gmail.com>
2024-05-04 20:08:56 -0300

    refactor transactions

--- lib/wallet/transactions.ex
+++ lib/wallet/transactions.ex
@@ -3,75 +3,85 @@ defmodule Wallet.Transactions do
   alias Wallet.Wallet
 
   def add_to_wallet_by_user(user_id, amount) do
-    case validate_amount(amount) do
-      :ok ->
-        case Wallets.get_wallet_by_user(user_id) do
-          nil -> {:error, {:not_found, "Wallet not found"}}
-          wallet ->
-            new_balance = Decimal.add(wallet.balance, Decimal.new(Float.to_string(amount)))
-
-            Wallet.changeset(wallet, %{balance: new_balance})
-            |> Wallets.update()
-        end
-
-      {:error, _} = error ->
-        error
+    with :ok <- validate_amount(amount),
+         {:ok, wallet} <- get_wallet_by_user(user_id),
+         {:ok, new_balance} <- calculate_new_balance(wallet, amount, :add) do
+      update_wallet_balance(wallet, new_balance)
+    else
+      error -> error
     end
   end
 
   def withdraw_to_wallet_by_user(user_id, amount) do
-    case validate_amount(amount) do
-      :ok ->
-        case Wallets.get_wallet_by_user(user_id) do
-          nil -> {:error, {:not_found, "Wallet not found"}}
-          wallet ->
-            new_balance = Decimal.sub(wallet.balance, Decimal.new(Float.to_string(amount)))
-
-            if Decimal.compare(new_balance, Decimal.new("0.0")) == :lt do
-              {:error, {:invalid, "Insufficient funds"}}
-            else
-              Wallet.changeset(wallet, %{balance: new_balance})
-              |> Wallets.update()
-            end
-        end
-
-      {:error, _} = error ->
-        error
+    with :ok <- validate_amount(amount),
+         {:ok, wallet} <- get_wallet_by_user(user_id),
+         {:ok, new_balance} <- calculate_new_balance(wallet, amount, :sub),
+         :ok <- validate_sufficient_funds(new_balance) do
+      update_wallet_balance(wallet, new_balance)
+    else
+      error -> error
     end
   end
 
   def tranfer_to_wallet_by_user(user_id, to_wallet_number, amount) do
-    case validate_amount(amount) do
-      :ok ->
-        case Wallets.get_wallet_by_user(user_id) do
-          nil -> {:error, {:not_found, "Wallet not found"}}
-          from_wallet ->
-            case Wallets.get_wallet_by_number(to_wallet_number) do
-              nil -> {:error, {:not_found, "To wallet not found"}}
-              to_wallet ->
-                if from_wallet.id == to_wallet.id do
-                  {:error, {:invalid, "Same wallet transfer not allowed"}}
-                else
-                  new_balance_from = Decimal.sub(from_wallet.balance, Decimal.new(Float.to_string(amount)))
+    with :ok <- validate_amount(amount),
+         {:ok, from_wallet} <- get_wallet_by_user(user_id),
+         {:ok, to_wallet} <- get_wallet_by_number(to_wallet_number),
+         :ok <- validate_same_wallet(from_wallet, to_wallet),
+         {:ok, new_balance_from} <- calculate_new_balance(from_wallet, amount, :sub),
+         :ok <- validate_sufficient_funds(new_balance_from),
+         {:ok, new_balance_to} <- calculate_new_balance(to_wallet, amount, :add) do
+          update_wallet_balance(to_wallet, new_balance_to)
+          update_wallet_balance(from_wallet, new_balance_from)
 
-                  if Decimal.compare(new_balance_from, Decimal.new("0.0")) == :lt do
-                    {:error, {:invalid, "Insufficient funds"}}
-                  else
-                    new_balance_to = Decimal.add(to_wallet.balance, Decimal.new(Float.to_string(amount)))
-
-                    Wallet.changeset(to_wallet, %{balance: new_balance_to})
-                    |> Wallets.update()
-
-                    Wallet.changeset(from_wallet, %{balance: new_balance_from})
-                    |> Wallets.update()
-                  end
-                end
-            end
-        end
-
-      {:error, _} = error ->
-        error
+    else
+      error -> error
     end
+  end
+
+  defp get_wallet_by_user(user_id) do
+    case Wallets.get_wallet_by_user(user_id) do
+      nil -> {:error, {:not_found, "Wallet not found"}}
+      wallet -> {:ok, wallet}
+    end
+  end
+
+  defp get_wallet_by_number(wallet_number) do
+    case Wallets.get_wallet_by_number(wallet_number) do
+      nil -> {:error, {:not_found, "To wallet not found"}}
+      wallet -> {:ok, wallet}
+    end
+  end
+
+  defp validate_same_wallet(from_wallet, to_wallet) do
+    if from_wallet.id == to_wallet.id do
+      {:error, {:invalid, "Same wallet transfer not allowed"}}
+    else
+      :ok
+    end
+  end
+
+  defp calculate_new_balance(wallet, amount, operation) do
+    new_balance =
+      case operation do
+        :sub -> Decimal.sub(wallet.balance, Decimal.new(Float.to_string(amount)))
+        :add -> Decimal.add(wallet.balance, Decimal.new(Float.to_string(amount)))
+      end
+
+    {:ok, new_balance}
+  end
+
+  defp validate_sufficient_funds(new_balance) do
+    if Decimal.compare(new_balance, Decimal.new("0.0")) == :lt do
+      {:error, {:invalid, "Insufficient funds"}}
+    else
+      :ok
+    end
+  end
+
+  defp update_wallet_balance(wallet, new_balance) do
+    Wallet.changeset(wallet, %{balance: new_balance})
+    |> Wallets.update()
   end
 
   defp correct_decimal_places?(amount) do
