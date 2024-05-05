eb5033771d05a1b312a11135744c34679163d5fc
Hebert <hebertsouza87@gmail.com>
2024-05-04 22:36:42 -0300

    Add lock to wallent on transacions

--- lib/wallet/transactions.ex
+++ lib/wallet/transactions.ex
@@ -1,12 +1,13 @@
 defmodule Wallet.Transactions do
+  alias Ecto.Multi
   alias Wallet.Repo
   alias Wallet.Wallets
   alias Wallet.Wallet
 
   def add_to_wallet_by_user(user_id, amount) do
     with :ok <- validate_amount(amount),
-         {:ok, wallet} <- get_wallet_by_user(user_id),
-         {:ok, new_balance} <- calculate_new_balance(wallet, amount, :add) do
+         {:ok, wallet} <- Wallets.get_and_lock_wallet_by_user(user_id),
+         new_balance <- calculate_new_balance(wallet, amount, :add) do
       update_wallet_balance(wallet, new_balance)
     else
       error -> error
@@ -15,8 +16,8 @@ defmodule Wallet.Transactions do
 
   def withdraw_to_wallet_by_user(user_id, amount) do
     with :ok <- validate_amount(amount),
-         {:ok, wallet} <- get_wallet_by_user(user_id),
-         {:ok, new_balance} <- calculate_new_balance(wallet, amount, :sub),
+         {:ok, wallet} <- Wallets.get_and_lock_wallet_by_user(user_id),
+         new_balance <- calculate_new_balance(wallet, amount, :sub),
          :ok <- validate_sufficient_funds(new_balance) do
       update_wallet_balance(wallet, new_balance)
     else
@@ -24,22 +25,22 @@ defmodule Wallet.Transactions do
     end
   end
 
-  def tranfer_to_wallet_by_user(user_id, to_wallet_number, amount) do
+  def transfer_to_wallet_by_user(user_id, to_wallet_number, amount) do
     with :ok <- validate_amount(amount),
-         {:ok, from_wallet} <- get_wallet_by_user(user_id),
-         {:ok, to_wallet} <- get_wallet_by_number(to_wallet_number),
+         {:ok, from_wallet} <- Wallets.get_and_lock_wallet_by_user(user_id),
+         {:ok, to_wallet} <- Wallets.get_and_lock_wallet_by_number(to_wallet_number),
          :ok <- validate_same_wallet(from_wallet, to_wallet),
-         {:ok, new_balance_from} <- calculate_new_balance(from_wallet, amount, :sub),
+         new_balance_from <- calculate_new_balance(from_wallet, amount, :sub),
          :ok <- validate_sufficient_funds(new_balance_from),
-         {:ok, new_balance_to} <- calculate_new_balance(to_wallet, amount, :add) do
+         new_balance_to <- calculate_new_balance(to_wallet, amount, :add) do
 
       from_wallet_changeset = Ecto.Changeset.change(from_wallet, %{balance: new_balance_from})
       to_wallet_changeset = Ecto.Changeset.change(to_wallet, %{balance: new_balance_to})
 
       multi =
-        Ecto.Multi.new()
-        |> Ecto.Multi.update(:update_to, to_wallet_changeset)
-        |> Ecto.Multi.update(:update_from, from_wallet_changeset)
+        Multi.new()
+        |> Multi.update(:update_to, to_wallet_changeset)
+        |> Multi.update(:update_from, from_wallet_changeset)
 
       case Repo.transaction(multi) do
         {:ok, %{update_from: updated_from_wallet}} ->
@@ -53,20 +54,6 @@ defmodule Wallet.Transactions do
     end
   end
 
-  defp get_wallet_by_user(user_id) do
-    case Wallets.get_wallet_by_user(user_id) do
-      nil -> {:error, {:not_found, "Wallet not found"}}
-      wallet -> {:ok, wallet}
-    end
-  end
-
-  defp get_wallet_by_number(wallet_number) do
-    case Wallets.get_wallet_by_number(wallet_number) do
-      nil -> {:error, {:not_found, "To wallet not found"}}
-      wallet -> {:ok, wallet}
-    end
-  end
-
   defp validate_same_wallet(from_wallet, to_wallet) do
     if from_wallet.id == to_wallet.id do
       {:error, {:invalid, "Same wallet transfer not allowed"}}
@@ -75,15 +62,8 @@ defmodule Wallet.Transactions do
     end
   end
 
-  defp calculate_new_balance(wallet, amount, operation) do
-    new_balance =
-      case operation do
-        :sub -> Decimal.sub(wallet.balance, Decimal.new(Float.to_string(amount)))
-        :add -> Decimal.add(wallet.balance, Decimal.new(Float.to_string(amount)))
-      end
-
-    {:ok, new_balance}
-  end
+  defp calculate_new_balance(wallet, amount, :sub) do Decimal.sub(wallet.balance, Decimal.new(Float.to_string(amount))) end
+  defp calculate_new_balance(wallet, amount, :add) do Decimal.add(wallet.balance, Decimal.new(Float.to_string(amount))) end
 
   defp validate_sufficient_funds(new_balance) do
     if Decimal.compare(new_balance, Decimal.new("0.0")) == :lt do
--- lib/wallet/wallets.ex
+++ lib/wallet/wallets.ex
@@ -1,4 +1,5 @@
 defmodule Wallet.Wallets do
+  import Ecto.Query
   alias Wallet.Repo
   alias Wallet.Wallet
 
@@ -17,10 +18,32 @@ defmodule Wallet.Wallets do
   end
 
   def get_wallet_by_user(user_id) do
-    Repo.get_by(Wallet, user_id: user_id)
+    case Repo.get_by(Wallet, user_id: user_id) do
+      nil -> {:error, {:not_found, "Wallet not found"}}
+      wallet -> {:ok, wallet}
+    end
   end
 
-  def get_wallet_by_number(number) do
-    Repo.get_by(Wallet, number: number)
+  def get_wallet_by_number(wallet_number) do
+    case Repo.get_by(Wallet, number: wallet_number) do
+      nil -> {:error, {:not_found, "Wallet not found"}}
+      wallet -> {:ok, wallet}
+    end
+  end
+
+  def get_and_lock_wallet_by_user(user_id) do
+    query = from w in Wallet, where: w.user_id == ^user_id, lock: "FOR UPDATE"
+    case Repo.one(query) do
+      nil -> {:error, "Wallet not found"}
+      wallet -> {:ok, wallet}
+    end
+  end
+
+  def get_and_lock_wallet_by_number(wallet_number) do
+    query = from w in Wallet, where: w.number == ^wallet_number, lock: "FOR UPDATE"
+    case Repo.one(query) do
+      nil -> {:error, "Wallet not found"}
+      wallet -> {:ok, wallet}
+    end
   end
 end
