c7a4676ed2de2c1c7dd1afeb019182cc6f023b77
Hebert <hebertsouza87@gmail.com>
2024-05-05 15:31:18 -0300

    Add DevAuxControllerTest

---
+++ test/wallet_web/controllers/dev_aux_controller_test.exs
@@ -0,0 +1,17 @@
+defmodule WalletWeb.DevAuxControllerTest do
+  use WalletWeb.ConnCase
+  alias Authentication.Helper
+  import WalletWeb.Endpoint, warn: false
+
+  setup do
+    user_id = "d02f68d3-079b-4f5b-93ac-2628d0b624fb"
+    {:ok, user_id: user_id}
+  end
+
+  test "creates a valid JWT token", %{conn: conn, user_id: user_id} do
+    conn = conn
+           |> post("/api/dev/token", %{"user_id" => user_id})
+
+    assert json_response(conn, 201)["token"] != nil
+  end
+end
