bfe67ede8d0cff65b78154b30fa9765fa3549696
Hebert Souza <hebertsouza87@gmail.com>
2024-05-07 16:28:21 -0300

    refactor: Update create_wallet function to accept user_id as a tuple

--- test/wallet/wallets_test.exs
+++ test/wallet/wallets_test.exs
@@ -4,51 +4,45 @@ defmodule Wallet.WalletsTest do
   alias Wallet.Wallets
   alias Wallet.Wallet
 
-  @valid_attrs %{user_id: "818f2e77-3610-4c74-939d-ea67ac2f2ef2"}
-  @invalid_attrs %{user_id: nil}
+  @valid_user_id "818f2e77-3610-4c74-939d-ea67ac2f2ef2"
 
   describe "Wallets" do
     test "create_wallet/1 with valid data creates a wallet" do
-      assert {:ok, %Wallet{} = wallet} = Wallets.create_wallet(@valid_attrs)
-      assert wallet.user_id == "818f2e77-3610-4c74-939d-ea67ac2f2ef2"
+      assert {:ok, %Wallet{} = wallet} = Wallets.create_wallet({:ok, @valid_user_id})
+      assert wallet.user_id == @valid_user_id
       assert wallet.number != nil
     end
 
-    test "create_wallet/1 with invalid data returns an error" do
-      assert {:bad_request, _changeset} = Wallets.create_wallet(@invalid_attrs)
-    end
-
     test "create_wallet/1 fails when wallet already exists for user_id" do
-      Wallets.create_wallet(@valid_attrs)
-      assert {:bad_request, _message} = Wallets.create_wallet(@valid_attrs)
+      Wallets.create_wallet({:ok, @valid_user_id})
+      assert {:error, _message} = Wallets.create_wallet({:ok, @valid_user_id})
     end
 
     test "update/1 with valid changeset updates the wallet" do
-      {:ok, wallet} = Wallets.create_wallet(@valid_attrs)
+      {:ok, wallet} = Wallets.create_wallet({:ok, @valid_user_id})
       changeset = Wallet.changeset(wallet, %{balance: 10.00})
       assert {:ok, %Wallet{} = updated_wallet} = Wallets.update(changeset)
       assert updated_wallet.balance == Decimal.new("10.0")
     end
 
     test "update/1 with balance having more than two decimal places returns an error" do
-      {:ok, wallet} = Wallets.create_wallet(@valid_attrs)
+      {:ok, wallet} = Wallets.create_wallet({:ok, @valid_user_id})
       changeset = Wallet.changeset(wallet, %{balance: 10.001})
       assert {:bad_request, _message} = Wallets.update(changeset)
     end
 
     test "get_wallet_by_user returns wallet for existing user_id" do
-      {:ok, wallet} = Wallets.create_wallet(@valid_attrs)
-      assert {:ok, %Wallet{}} = Wallets.get_wallet_by_user(wallet.user_id)
+      {:ok, wallet} = Wallets.create_wallet({:ok, @valid_user_id})
+      assert {:ok, %Wallet{}} = Wallets.get_wallet_by_user({:ok, wallet.user_id})
     end
 
     test "get_wallet_by_user returns error for non-existing user_id" do
-      assert_raise Ecto.Query.CastError, fn ->
-        Wallets.get_wallet_by_user("non-existing-id")
-      end
+      non_existing_user_id = "00000000-0000-0000-0000-000000000000"
+      assert {:not_found, _} = Wallets.get_wallet_by_user(non_existing_user_id)
     end
 
     test "get_wallet_by_number returns wallet for existing number" do
-      {:ok, wallet} = Wallets.create_wallet(@valid_attrs)
+      {:ok, wallet} = Wallets.create_wallet({:ok, @valid_user_id})
       assert {:ok, %Wallet{}} = Wallets.get_wallet_by_number(wallet.number)
     end
 
@@ -57,7 +51,7 @@ defmodule Wallet.WalletsTest do
     end
 
     test "get_and_lock_wallet_by_user locks and returns the wallet for a given user_id" do
-      {:ok, wallet} = Wallets.create_wallet(@valid_attrs)
+      {:ok, wallet} = Wallets.create_wallet({:ok, @valid_user_id})
       assert {:ok, %Wallet{}} = Wallets.get_and_lock_wallet_by_user(wallet.user_id)
     end
 
@@ -67,7 +61,7 @@ defmodule Wallet.WalletsTest do
     end
 
     test "get_and_lock_wallet_by_number locks and returns the wallet for a given wallet number" do
-      {:ok, wallet} = Wallets.create_wallet(@valid_attrs)
+      {:ok, wallet} = Wallets.create_wallet({:ok, @valid_user_id})
       assert {:ok, %Wallet{}} = Wallets.get_and_lock_wallet_by_number(wallet.number)
     end
 
